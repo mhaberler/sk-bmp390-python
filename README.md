b48d313cd33845a5b3bdca7101b4bb1775174657
Michael Haberler <git@mah.priv.at>
2022-06-05 13:20:38 +0200

    use BMP3xx library for pressure, temperature

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# sk-plugin-python-demo
-Demonstration of a SK server plugin in Python
+# sk-bmp390-python
+SK server plugin in Python for reading Bosch BMP3xx sensors via 
+Adafruit BMP 3xx library
 
 ## Development
 
 - clone the plugin from Github
 - `npm link` in the plugin directory
-- `npm link sk-plugin-python-demo` in your server directory
+- `npm link sk-bmp390-python` in your server directory
 
 
+## BMP390
+- used Adafruit BM390 breakout
+- install Adafruit Python driver: pip3 install adafruit-circuitpython-bmp3xx
--- plugin.py
+++ plugin.py
@@ -1,22 +1,38 @@
-import sys, json, threading;
+import sys, json, threading
+import board
+import adafruit_bmp3xx
 
-n = 0
+
 def outputSk():
     global n
-    skData = {'updates': [{ 'values': [{'path': 'some.path', 'value': n}]}]}
-    sys.stdout.write(json.dumps(skData) + '\n' + json.dumps(skData))
-    sys.stdout.write('\n')
+    skData = {
+        "updates": [
+            {
+                "values": [
+                    {"path": "environment.bmp390.pressure", "value": bmp.pressure},
+                    {"path": "environment.bmp390.temperature", "value": bmp.temperature},
+                ]
+            }
+        ]
+    }
+    sys.stdout.write(json.dumps(skData))
+    sys.stdout.write("\n")
     sys.stdout.flush()
-    n += 1
     threading.Timer(1.0, outputSk).start()
+
+
+# I2C setup
+i2c = board.I2C()  # uses board.SCL and board.SDA
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+bmp.pressure_oversampling = 8
+bmp.temperature_oversampling = 2
 
 threading.Timer(1.0, outputSk).start()
 
-for line in iter(sys.stdin.readline, b''):
+for line in iter(sys.stdin.readline, b""):
     try:
         data = json.loads(line)
         sys.stderr.write(json.dumps(data))
     except:
-        sys.stderr.write('error parsing json\n')
+        sys.stderr.write("error parsing json\n")
         sys.stderr.write(line)
-
